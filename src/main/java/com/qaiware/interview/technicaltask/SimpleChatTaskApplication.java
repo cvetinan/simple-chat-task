0e00fbf70c00ef4f5624ef2171612c47f5b55ce0
Tsvetina Nencheva <cvetina.nencheva@gmail.com>
2022-09-03 14:42:30 +0300

    Added SimpleChatTaskApplication

---
+++ src/main/java/com/qaiware/interview/technicaltask/SimpleChatTaskApplication.java
@@ -0,0 +1,15 @@
+package com.qaiware.interview.technicaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SimpleChatTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleChatTaskApplication.class, args);
+    }
+
+}
